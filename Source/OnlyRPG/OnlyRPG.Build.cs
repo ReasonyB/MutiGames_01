ad66ad862f8546371b04588da04d3f646305ac40
ReasonyB <ydlwnsj25@gmail.com>
2024-04-06 01:12:46 +0900

    Base Project

---
+++ Source/OnlyRPG/OnlyRPG.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class OnlyRPG : ModuleRules
+{
+	public OnlyRPG(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput" });
+	}
+}
